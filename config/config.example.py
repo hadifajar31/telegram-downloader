44b2c875572a851b78686e39b5ec84c3772b5a35
hadifajar31 <hadifajar3130@gmail.com>
2026-04-25 22:06:21 +0700

    init: clean project structure

---
+++ config/config.example.py
@@ -0,0 +1,6 @@
+import os
+
+api_id = 0          # isi dengan api_id dari my.telegram.org
+api_hash = ""       # isi dengan api_hash dari my.telegram.org
+
+base_folder = os.path.join(os.path.expanduser("~"), "Downloads", "Tele")
